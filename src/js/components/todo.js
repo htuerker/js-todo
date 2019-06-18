fa6abe5a75fac65a653eb8c63cd26f540a2b6cac
madcido <fabowow@gmail.com>
2019-06-18 16:25:04 -0300

    Basic structure

---
+++ src/js/components/todo.js
@@ -0,0 +1,22 @@
+import Swal from 'sweetalert2';
+
+export default function newTodoForm() {
+    Swal.fire({
+        html:
+        '<input id="title" class="swal2-input" placeholder="Title">'
+        ,
+        focusConfirm: false,
+        confirmButtonText: 'Add To-do',
+        preConfirm: () => {
+            newTodo();
+        }
+    });
+}
+
+function newTodo() {
+    const newTodo = document.createElement("div");
+    newTodo.innerHTML = document.getElementById("title").value;
+    newTodo.className = "to-do";
+    const projects = document.getElementById('to-dos');
+    projects.appendChild(newTodo);
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,21 @@
 import '../styles/main.css';
+import newProjectForm from './components/project';
+import newTodoForm from './components/todo';
 
-alert('hello, world!');
+function init() {
+    const newProjectButton = document.createElement('button');
+    newProjectButton.innerHTML = 'New Project';
+    newProjectButton.addEventListener('click', () => newProjectForm());
+
+    const projects = document.getElementById('projects');
+    projects.appendChild(newProjectButton);
+
+    const newTodoButton = document.createElement('button');
+    newTodoButton.innerHTML = 'New To-do';
+    newTodoButton.addEventListener('click', () => newTodoForm());
+
+    const todos = document.getElementById('to-dos');
+    todos.appendChild(newTodoButton);
+}
+
+init();
