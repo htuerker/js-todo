73366c4abd51ac0bdf2287bb76c3ce52fd7c0d7e
madcido <fabowow@gmail.com>
2019-06-20 20:38:45 -0300

    Add draggable scroll

--- src/js/components/ProjectList.js
+++ src/js/components/ProjectList.js
@@ -3,54 +3,76 @@ import Project from './Project';
 import Todo from './Todo';
 
 export default function ProjectList() {
-  const projects = retrieveProjects();
-  const projectsDiv = document.createElement('div');
-  projectsDiv.className = 'projects';
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    
+    const projects = retrieveProjects();
+    const projectsDiv = document.createElement('div');
+    projectsDiv.className = 'projects';
+    projectsDiv.addEventListener('mousedown', (e) => {
+        isDown = true;
+        startX = e.pageX - projectsDiv.offsetLeft;
+        scrollLeft = projectsDiv.scrollLeft;
+    });
+    projectsDiv.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+    projectsDiv.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+    projectsDiv.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - projectsDiv.offsetLeft;
+        const walk = (x - startX) * 1;
+        projectsDiv.scrollLeft = scrollLeft - walk;
+    });
 
-  projects.forEach((project) => {
-    Object.setPrototypeOf(project, Project.prototype)
-    projectsDiv.appendChild(project.render());
-  });
+    projects.forEach((project) => {
+        Object.setPrototypeOf(project, Project.prototype)
+        projectsDiv.appendChild(project.render());
+    });
 
-  const nextButton = document.createElement('i');
-  nextButton.className = "fas fa-arrow-right"
-  nextButton.id = 'next-btn';
-  nextButton.addEventListener('click', () => plusSlider(1));
+    const nextButton = document.createElement('i');
+    nextButton.className = "fas fa-arrow-right"
+    nextButton.id = 'next-btn';
+    nextButton.addEventListener('click', () => plusSlider(1));
 
-  const prevButton = document.createElement('i');
-  prevButton.className = "fas fa-arrow-left"
-  prevButton.id = 'prev-btn';
-  prevButton.addEventListener('click', () => plusSlider(-1));
+    const prevButton = document.createElement('i');
+    prevButton.className = "fas fa-arrow-left"
+    prevButton.id = 'prev-btn';
+    prevButton.addEventListener('click', () => plusSlider(-1));
 
-  projectsDiv.prepend(nextButton);
-  projectsDiv.prepend(prevButton);
+    projectsDiv.prepend(nextButton);
+    projectsDiv.prepend(prevButton);
 
-  return projectsDiv;
+    return projectsDiv;
 }
 
 function plusSlider(n) {
-  let x = document.querySelector(".projects");
-  let target = x.scrollLeft + 342 * n;
+    let x = document.querySelector(".projects");
+    let target = x.scrollLeft + 342 * n;
 
-  let prev = 0;
-  var id = setInterval(frame, 3);
-  function frame() {
-    if(n > 0) {
-      if (x.scrollLeft >= target || x.scrollLeft == x.scrollWidth) {
-        clearInterval(id);
-      }
-    } else {
-      if (x.scrollLeft <= target || x.scrollLeft == 0) {
-        clearInterval(id);
-      }
-    }
+    let prev = 0;
+    var id = setInterval(frame, 3);
+    function frame() {
+        if(n > 0) {
+            if (x.scrollLeft >= target || x.scrollLeft == x.scrollWidth) {
+                clearInterval(id);
+            }
+        } else {
+            if (x.scrollLeft <= target || x.scrollLeft == 0) {
+                clearInterval(id);
+            }
+        }
 
-    let current = x.scrollLeft;
-    x.scrollLeft += (9 * n);
-    if(x.scrollLeft == prev) {
-      clearInterval(id);
-    } else {
-      prev = current;
+        let current = x.scrollLeft;
+        x.scrollLeft += (9 * n);
+        if(x.scrollLeft == prev) {
+            clearInterval(id);
+        } else {
+            prev = current;
+        }
     }
-  }
 }
